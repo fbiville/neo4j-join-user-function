34d91675e95447e15658378a077e5997ed8b3865
Florent Biville <florent.biville@neo4j.com>
2020-10-08 15:38:34 +0200

    Move to Junit 5 and fix project metadata

--- src/test/java/example/JoinTest.java
+++ src/test/java/example/JoinTest.java
@@ -1,30 +1,40 @@
 package example;
 
 import org.hamcrest.CoreMatchers;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
-import org.neo4j.harness.junit.rule.Neo4jRule;
+import org.neo4j.harness.Neo4j;
+import org.neo4j.harness.junit.extension.Neo4jExtension;
+
+import java.util.logging.LogManager;
 
 import static org.junit.Assert.assertThat;
 
 
 public class JoinTest {
 
-    @Rule
-    public Neo4jRule neo4j = new Neo4jRule().withFunction(Join.class);
+    // mute logging
+    static {
+        LogManager.getLogManager().reset();
+    }
+
+    // alternatively, use org.neo4j.harness.Neo4jBuilders to manually instantiate a Neo4j instance
+    // and have tighter control of the Neo4j instance lifecycle
+    @RegisterExtension
+    static Neo4jExtension extension = Neo4jExtension.builder()
+            .withFunction(Join.class)
+            .build();
+
+    Config unencrypted = Config.builder().withoutEncryption().build();
 
     @Test
-    public void joinsStrings() {
-        Config config = Config.builder()
-                .withoutEncryption()
-                .build();
-
-        try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), config)) {
+    void joins_strings(Neo4j neo4j) {
+        try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), unencrypted)) {
             Session session = driver.session();
             Result result = session.run("RETURN example.join(['A','quick','brown','fox'],' ') as sentence");
             assertThat(result.single().get("sentence").asString(), CoreMatchers.equalTo("A quick brown fox"));
